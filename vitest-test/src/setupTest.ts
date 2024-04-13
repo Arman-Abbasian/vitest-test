7a64c957e24078c60739053a96fa17fd9cc2230d
Arman Abbasian <a.abbasian@avaparsi.com>
2024-04-13 09:22:21 +0330

    add MSW to the test

--- vitest-test/src/setupTest.ts
+++ vitest-test/src/setupTest.ts
@@ -1,3 +1,11 @@
 /// <reference types="vitest/globals" />
 import '@testing-library/jest-dom';
 import "@testing-library/jest-dom/extend-expect";
+import { server } from './components/Button/components/test/mocks/server';
+
+
+
+
+beforeAll(() => server.listen({ onUnhandledRequest: 'error' }))
+afterAll(() => server.close())
+afterEach(() => server.resetHandlers())
